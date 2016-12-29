b551e3124b406dbcde8d20b8c48eeed510e33c13
Isaiah Mann <contact@isaiahmann.com>
2016-12-28 20:14:55 -0500

    Adding opacity lerp and transitioning between scenes

---
+++ Assets/Scripts/UI/Core/UIOpacityLerpAction.cs
@@ -0,0 +1,18 @@
+﻿/*
+ * Author: Isaiah Mann
+ * Description: Lerps an assets opacity in and out
+ */
+
+using UnityEngine;
+
+[CreateAssetMenuAttribute(fileName = "OpacityLerp", menuName = "UIEvent/OpacityLerp", order = 3)]
+public class UIOpacityLerpAction : UIAction {
+	public float StartOpacity;
+	public float EndOpacity;
+	public float Time;
+	public bool Loop;
+
+	public override void Execute (UIElement target) {
+		target.StartOpacityLerp(StartOpacity, EndOpacity, Time, Loop);
+	}
+}
---
+++ Assets/Scripts/UI/Pickup Pup/PPUIController.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Author: Isaiah Mann
+ * Description: Controls a UI
+ */
+
+public class PPUIController : MonoBehaviourExtended {
+	protected PPSceneController scene;
+
+	protected override void FetchReferences () {
+		base.FetchReferences ();
+		scene = PPSceneController.Instance;
+	}
+
+	public void LoadStart () {
+		scene.LoadStart();
+	}
+
+	public void LoadHome () {
+		scene.LoadHome();
+	}
+}
