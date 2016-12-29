8e64c1810c7204ee1789b3e7f94690a84c525818
Grace Barrett-Snyder <gbarrettsnyder@smith.edu>
2016-12-29 15:47:06 -0500

    Added dog home display

---
+++ Assets/Scripts/Controller/Pickup Pup/HomeController.cs
@@ -0,0 +1,56 @@
+﻿/*
+ * Author(s): Grace Barrett-Snyder 
+ * Description: Controls adjustments to the Player's Home
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomeController : SingletonController<HomeController> {
+
+    DogDatabase dogData;
+    DogDescriptor[] dogs;
+
+    int numOfSlots = 10;
+    int numOfAvailableSlots = 0;
+    int numOfDogsHome = 0;
+
+    public void Init()
+    {
+        dogData = ((PPGameController) PPGameController.Instance).Data;
+        dogs = dogData.Dogs;
+    }
+
+    public void addSlot()
+    {
+        numOfSlots++;
+        numOfAvailableSlots++;
+    }
+
+    public void removeSlot()
+    {
+        numOfSlots--;
+        if (numOfAvailableSlots > 0)
+        {
+            numOfAvailableSlots--;
+        }
+    }
+
+	public void addDog()
+    {
+        numOfAvailableSlots--;
+        numOfDogsHome++;
+    }
+
+    public void removeDog()
+    {
+        numOfAvailableSlots++;
+        numOfDogsHome--;
+    }
+
+    public DogDescriptor[] Dogs
+    {
+        get { return dogs; }
+    }
+}
--- Assets/Scripts/Controller/Pickup Pup/PPGameController.cs
+++ Assets/Scripts/Controller/Pickup Pup/PPGameController.cs
@@ -7,12 +7,15 @@ using System.IO;
 using UnityEngine;
 
 public class PPGameController : GameController {
+
 	const string JSON_DIR = "JSON";
+
 	static string GAME_DATA_FILE_PATH {
 		get {
 			return Path.Combine(JSON_DIR, "GameData");
 		}
 	}
+
 	DogDatabase data;
 
 	protected override void SetReferences () {
@@ -20,4 +23,9 @@ public class PPGameController : GameController {
 		data = JsonUtility.FromJson<DogDatabase>(Resources.Load<TextAsset>(GAME_DATA_FILE_PATH).text);
 		data.Initialize();
 	}
+
+    public DogDatabase Data
+    {
+        get { return data; }
+    }
 }
