e865849442d7b637f8484c5d133a2f7560a2947a
Isaiah Mann <contact@isaiahmann.com>
2017-01-05 07:54:29 -0500

    Adding list backing structure

--- Assets/Scripts/Data/Core/RandomBuffer.cs
+++ Assets/Scripts/Data/Core/RandomBuffer.cs
@@ -4,51 +4,83 @@
  */
 
 using System;
+using System.Collections;
 using System.Collections.Generic;
 
-public class RandomBuffer<T> {	
-	T[] source;
+public class RandomBuffer<T> 
+{	
+	
+	protected virtual IList<T> source 
+	{
+		get 
+		{
+			return backingSource;
+		}
+	}
+
+	T[] backingSource;
 	Random random;
 	// Using a hash set for fast lookup
 	HashSet<int> usedIndices;
 
-	public RandomBuffer (T[] source) {
-		this.source = source;
+	public RandomBuffer(T[] source) 
+	{
+		this.backingSource = source;
+		setup();
+	}
+
+	protected RandomBuffer()
+	{
+		setup();
+	}
+
+	void setup()
+	{
 		random = new Random();
 		usedIndices = new HashSet<int>();
 	}
 
 	// Adapted from: http://codereview.stackexchange.com/questions/61338/generate-random-numbers-without-repetitions
-	int nextRandomIndex () {
+	int nextRandomIndex() 
+	{
 		// Avoids creating an infinite loop by dumping all of the used indices (once all indices have been used)
-		if (needsRefresh()) {
+		if(needsRefresh()) 
+		{
 			refresh();
 		}
 		int index;
-		do {
-			index = random.Next(source.Length);
-		} while (usedIndices.Contains(index));
+		do 
+		{
+			index = random.Next(source.Count);
+		}
+		while(usedIndices.Contains(index));
 		usedIndices.Add(index);
 		return index;
 	}
 
-	public T GetRandom () {
+	public T GetRandom() 
+	{
 		return source[nextRandomIndex()];
 	}
 
-	public T[] GetRandom (int count) {
+	public T[] GetRandom(int count) 
+	{
 		T[] randomList = new T[count];
-		for (int i = 0; i < count; i++) {
+		for(int i = 0; i < count; i++) 
+		{
 			randomList[i] = GetRandom();
 		}
 		return randomList;
 	}
 
-	bool needsRefresh () {
-		return usedIndices.Count >= source.Length;
+	bool needsRefresh() 
+	{
+		return usedIndices.Count >= source.Count;
 	}
 			
-	void refresh () {
+	void refresh() 
+	{
 		usedIndices.Clear();
 	}
+
 }
---
+++ Assets/Scripts/Data/Core/RandomListBuffer.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Author: Isaiah Mann
+ * Desc: Random buffer with a list backing structure instead of an array
+ */
+
+using System.Collections.Generic;
+
+public class RandomListBuffer<T> : RandomBuffer<T>
+{
+
+	protected override IList<T> source
+	{
+		get
+		{
+			return backingSource;
+		}
+	}
+
+	List<T> backingSource;
+
+	public RandomListBuffer(IList<T> source)
+	{
+		backingSource = new List<T>(source);
+	}
+
+	public void AddToBuffer(T element) 
+	{
+		backingSource.Add(element);
+	}
+
+}
