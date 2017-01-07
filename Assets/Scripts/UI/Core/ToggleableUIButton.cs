ba56d300a1e8ea523df2a80678b87c5f96d1ce12
Isaiah Mann <contact@isaiahmann.com>
2017-01-07 14:58:53 -0500

    Working on the scripting functionality of the all dogs page

---
+++ Assets/Scripts/UI/Core/ToggleableUIButton.cs
@@ -0,0 +1,118 @@
+﻿/*
+ * Author(s): Isaiah Mann
+ * Description: Describes a button behaviour which can be toggled and off
+ */
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleableUIButton : UILabledButton 
+{
+	#region Instance Acccessors
+
+	public bool IsToggled 
+	{
+		get 
+		{
+			return toggled;
+		}
+	}
+
+	#endregion
+
+	Color buttonUnselectedColor;
+	Color buttonSelectedColor;
+
+	[SerializeField]
+	bool shouldToggleImage;
+	[SerializeField]
+	Image toggleImage;
+	[SerializeField]
+	Sprite imageOn;
+	[SerializeField]
+	Sprite imageOff;
+
+	[SerializeField]
+	bool shouldShowToggle;
+	bool toggled = false;
+	MonoAction toggleOffAction;
+	Image buttonImage;
+
+	public void SubscribeToggleOffAction(MonoAction toggleAction) 
+	{
+		toggleOffAction += toggleAction;
+	}
+
+	public void UnsubscribeToggleOffAction(MonoAction toggleAction) 
+	{
+		toggleOffAction -= toggleAction;
+	}
+		
+	public virtual void Toggle() 
+	{
+		toggled = !toggled;
+		if(shouldShowToggle) 
+		{
+			showToggle(toggled);
+		}
+	}
+
+	protected virtual void showToggle(bool isToggled)
+	{
+		if(isToggled) 
+		{
+			if(shouldToggleImage) 
+			{
+				toggleImage.sprite = imageOn;
+			}
+			buttonImage.color = buttonSelectedColor;
+		}
+		else 
+		{
+			if(shouldToggleImage) 
+			{
+				toggleImage.sprite = imageOff;
+			}
+			buttonImage.color = buttonUnselectedColor;
+		}
+	}
+
+	protected virtual void setToggle(bool isToggled)
+	{
+		toggled = isToggled;
+	}
+
+	protected override void executeClick() 
+	{
+		Toggle();
+		if(toggled)
+		{
+			base.executeClick();
+		} 
+		else 
+		{
+			executeToggleOff();
+		}
+	}
+
+	protected virtual void executeToggleOff()
+	{
+		if(toggleOffAction != null) 
+		{
+			toggleOffAction();
+		}
+	}
+
+	#region MonoBehaviourExtended Overrides
+
+	protected override void setReferences()
+	{
+		base.setReferences();
+		buttonImage = GetComponent<Image>();
+		buttonUnselectedColor = buttonImage.color;
+		buttonSelectedColor = Color.Lerp(buttonImage.color, Color.black, 0.5f);
+	}
+
+	#endregion
+
+}
--- Assets/Scripts/UI/Pickup Pup/DogBrowser.cs
+++ Assets/Scripts/UI/Pickup Pup/DogBrowser.cs
@@ -4,15 +4,26 @@
  * Usage: Should be attached to the parent element in the UI that holds the child elements
  */
 
+using UnityEngine;
+
 public class DogBrowser : PPUIElement 
 {	
+	[SerializeField]
+	int defaultStartPage;
 	DogSlot[] elements;
+	ToggleableColorUIButton[] pageButtons;
+
+	#region MonoBehaviourExtended 
 
 	protected override void setReferences()
 	{
 		base.setReferences();
 		elements = GetComponentsInChildren<DogSlot>();
+		pageButtons = GetComponentsInChildren<ToggleableColorUIButton>();
+		pageButtons[defaultStartPage].Toggle();
 	}
+
+	#endregion
 
 	public void Set(Dog[] dogs) 
 	{
@@ -20,6 +31,16 @@ public class DogBrowser : PPUIElement
 		{
 			elements[i].Init(dogs[i]);
 		}
+	}
+
+	public void SwitchToPage(int pageIndex)
+	{
+		throw new System.NotImplementedException();
+	}
+
+	protected Dog[] getDogsForPage(int pageIndex)
+	{
+		throw new System.NotImplementedException();
 	}
 
 }
