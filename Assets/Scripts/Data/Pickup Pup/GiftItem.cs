bcf83ce4c04f6356f458eb390b879e5e84fe89ce
Metkis <jameshostetler@live.com>
2017-01-12 05:28:36 -0500

    Missed Newline

--- Assets/Scripts/Data/Pickup Pup/GiftItem.cs
+++ Assets/Scripts/Data/Pickup Pup/GiftItem.cs
@@ -67,7 +67,6 @@ public class GiftItem : PPData
 
     #endregion
 
-
     [SerializeField]
     string giftName;
     [SerializeField]
--- Assets/Scripts/UI/Pickup Pup/GiftSlot/GiftRedeemSlot.cs
+++ Assets/Scripts/UI/Pickup Pup/GiftSlot/GiftRedeemSlot.cs
@@ -8,7 +8,6 @@ using UnityEngine.UI;
 
 public class GiftRedeemSlot : GiftSlot 
 {
-
     [SerializeField]
     Image itemImage;
 
