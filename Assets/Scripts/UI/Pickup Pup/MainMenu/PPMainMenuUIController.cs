917aa51ce98c782e73acc25e07da180179c0a9e6
Grace Barrett-Snyder <gbarrettsnyder@smith.edu>
2017-01-18 11:12:46 -0500

    Updated Main Menu UI to match wireframes

--- Assets/Scripts/UI/Pickup Pup/MainMenu/PPMainMenuUIController.cs
+++ Assets/Scripts/UI/Pickup Pup/MainMenu/PPMainMenuUIController.cs
@@ -1,10 +1,46 @@
 ﻿/*
- * Author(s): Isaiah Mann
+ * Authors: Isaiah Mann, Grace Barrett-Snyder
  * Description: Controls the behaviour of the main menu in Pickup Pup
  * Usage: [no notes]
  */
 
+using UnityEngine;
+
 public class PPMainMenuUIController : PPUIController 
 {
+    [SerializeField]
+    GameObject dogBrowserObject;
 
+    public void Close()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void OnShopClick()
+    {
+        sceneController.LoadShop();
+    }
+
+    public void OnAllDogsClick()
+    {
+        if (dogBrowserObject)
+        {
+            dogBrowserObject.SetActive(true);
+        }
+    }
+
+    public void OnSettingsClick()
+    {
+        // Disabled
+    }
+
+    public void OnGiftsClick()
+    {
+        // Disabled
+    }
+
+    public void OnWatchAdClick()
+    {
+        // Disabled
+    }
 }
