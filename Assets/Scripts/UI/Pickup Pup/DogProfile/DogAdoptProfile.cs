85ae2e99bfa8b3448c4930ac5a63610a663f8470
Grace Barrett-Snyder <gbarrettsnyder@smith.edu>
2017-01-19 15:35:46 -0500

    Added DogProfile to more scenes, added regions

--- Assets/Scripts/UI/Pickup Pup/DogProfile/DogAdoptProfile.cs
+++ Assets/Scripts/UI/Pickup Pup/DogProfile/DogAdoptProfile.cs
@@ -13,16 +13,24 @@ public class DogAdoptProfile : DogProfile
     [SerializeField]
     Button adoptButton;
 
+    #region MonoBehaviourExtended Overrides
+
     protected override void setReferences()
     {
         base.setReferences();
         iconsObject.SetActive(false);
     }
 
+    #endregion
+
+    #region DogProfile Overrides
+
     public override void SetProfile(Dog dog)
     {
         base.SetProfile(dog);
         priceText.text = dogInfo.CostToAdoptStr;
     }
+
+    #endregion
 
 }
