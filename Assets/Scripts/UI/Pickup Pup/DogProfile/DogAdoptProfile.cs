372d6a273100cb2c9371e7901eea02209e25b4e5
Grace Barrett-Snyder <gbarrettsnyder@smith.edu>
2017-01-19 15:25:17 -0500

    Updated dog profile UI, allowed more flexibility

--- Assets/Scripts/UI/Pickup Pup/DogProfile/DogAdoptProfile.cs
+++ Assets/Scripts/UI/Pickup Pup/DogProfile/DogAdoptProfile.cs
@@ -16,19 +16,13 @@ public class DogAdoptProfile : DogProfile
     protected override void setReferences()
     {
         base.setReferences();
-        rehomeButton.interactable = false;
-        collarSlot.interactable = false;
+        iconsObject.SetActive(false);
     }
 
     public override void SetProfile(Dog dog)
     {
         base.SetProfile(dog);
         priceText.text = dogInfo.CostToAdoptStr;
-    }
-
-    public void TryAdopt()
-    {
-        game.TryAdoptDog(dogInfo);
     }
 
 }
