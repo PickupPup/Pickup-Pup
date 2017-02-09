81a5e8cdb06a1e07567bd720eab1d34d2505a010
Timothy Ng <tdng@umass.edu>
2017-02-09 12:27:00 -0500

    Added a feeding controller to handle feeding logic

---
+++ Assets/Scripts/FeedingSystem/FeedingController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FeedingController : SingletonController<FeedingController> {
+
+    #region Instance Accessors
+
+    public float FeedTimerLeft 
+    {
+        get
+        {
+            return feedingTimer.TimeRemaining;
+        }
+    }
+
+    public bool IsCurrentlyFeeding
+    {
+        get
+        {
+            return feedingTimer.TimeRemaining != 0;
+        }
+    }
+
+    #endregion
+
+    [SerializeField]
+    PPTimer feedingTimer;
+
+    int calculateDogFoodNeeded()
+    {
+        return PPDataController.GetInstance.DogCount - PPDataController.GetInstance.ScoutingDogs.Count;
+    }
+
+    public bool TryFeedDogs()
+    {
+        if (PPDataController.GetInstance.CanAfford(CurrencyType.DogFood, calculateDogFoodNeeded()) && !IsCurrentlyFeeding)
+        {
+            feedDogs();
+            return true;
+        }
+        return false;
+    }
+
+    void feedDogs()
+    {
+        dataController.ChangeFood(-calculateDogFoodNeeded());
+        feedingTimer.SetTimeRemaining(20, false);
+
+        //TODO implement sprite change
+    }
+
+}
