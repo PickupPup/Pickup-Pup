e65f238ed0c8e256ac3c67406ec5925b91f89d83
Isaiah Mann <contact@isaiahmann.com>
2017-02-19 19:54:01 -0500

    Starting to work on displayed dogs logic

---
+++ Assets/Scripts/UI/PickupPup/DogWorld/DogHotSpot.cs
@@ -0,0 +1,40 @@
+﻿/*
+ * Author(s): Isaiah Mann
+ * Description: Dogs will travel / be displayed at this spot on the UI
+ * Usage: [no notes]
+ */
+
+using System.Linq;
+using System.Collections.Generic;
+
+public class DogHotSpot : PPUIElement
+{	
+    static HashSet<DogHotSpot> activeHotSpots = new HashSet<DogHotSpot>();
+
+    public static DogHotSpot[] GetActiveHotSpots()
+    {
+        return activeHotSpots.ToArray();
+    }
+
+    #region MonoBehaviourExtended Overrides 
+
+    protected override void setReferences()
+    {
+        base.setReferences();
+        activeHotSpots.Add(this);
+    }
+
+    protected override void cleanupReferences()
+    {
+        base.cleanupReferences();
+        activeHotSpots.Remove(this);
+    }
+
+    #endregion
+
+    public void PlaceDog(Dog dog)
+    {
+        // TODO: Implement
+    }
+
+}
---
+++ Assets/Scripts/UI/PickupPup/DogWorld/DogWorld.cs
@@ -0,0 +1,15 @@
+﻿/*
+ * Author(s): Isaiah Mann
+ * Description: [to be added]
+ * Usage: [no notes]
+ */
+
+public class DogWorld : PPUIController 
+{	
+    void populateHotSpots()
+    {
+        DogHotSpot[] spots = DogHotSpot.GetActiveHotSpots();
+        // TODO: Finish this by placing dogs in all these slots
+    }
+
+}
