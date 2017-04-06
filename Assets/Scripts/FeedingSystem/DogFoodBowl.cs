9d0bc25a3c0ac52a1892314cf936da11727a8253
Isaiah Mann <contact@isaiahmann.com>
2017-04-06 17:14:30 -0400

    Avoid filling dog food bowl if dog food cost is 0

--- Assets/Scripts/FeedingSystem/DogFoodBowl.cs
+++ Assets/Scripts/FeedingSystem/DogFoodBowl.cs
@@ -1,5 +1,5 @@
 ﻿/*
- * Author: Timothy Ng
+ * Authors: Timothy Ng, Isaiah Mann
  * Description: Handles the feeding dog code through calling FeedDogs
  */
 
@@ -82,7 +82,13 @@ public class DogFoodBowl : MonoBehaviourExtended
 
     public void FeedDogs()
     {
-        if(dataController.CanAfford(CurrencyType.DogFood, calculateDogFoodNeeded()) && !IsCurrentlyFeeding)
+		int foodNeeded = calculateDogFoodNeeded();
+		if(foodNeeded <= 0)
+		{
+			return;
+		}
+
+		if(dataController.CanAfford(CurrencyType.DogFood, foodNeeded) && !IsCurrentlyFeeding)
         {
             dataController.ChangeFood(-calculateDogFoodNeeded());
             feedingTimer.Reset();
