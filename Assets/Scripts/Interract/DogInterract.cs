c5e66d38b3e7ec2d671c8ff26c53aae82a3475bb
Timothy Ng <tdng@umass.edu>
2017-04-07 19:00:31 -0400

    interact

---
+++ Assets/Scripts/Interract/DogInterract.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DogInterract : MonoBehaviour {
+
+    Animator animator;
+
+    int affectionLevel = 0;
+    int tapCount = 0;
+
+    int hunger = 0;
+
+    [SerializeField]
+    Vector3 target = new Vector3(0, 0, 0);
+
+	void Start () {
+        animator = GetComponentInChildren<Animator>();
+
+    }
+
+    void Update () {
+        if (transform.position != target)
+        {
+            animator.SetBool("Moving", true);
+            Move();
+        }
+        else
+        {
+            animator.SetBool("Moving", false);
+        }
+    }
+
+    void OnMouseDown()
+    {
+        Pet();
+    }
+
+    public void Pet()
+    {
+        animator.SetTrigger("Pet");
+        tapCount++;
+        if(tapCount % 5 == 0)
+        {
+            affectionLevel++;
+        }
+    }
+
+    public void Idle()
+    {
+        animator.SetTrigger("Idle");
+    }
+
+    public void Eat()
+    {
+        animator.SetTrigger("Eat");
+        hunger = 0;
+    }
+
+    public void Move()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target, Time.deltaTime);
+    }
+}
