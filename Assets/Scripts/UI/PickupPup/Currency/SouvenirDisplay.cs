63739384fbcc3bc4ba51017f6f8f2a0e0e41979b
Isaiah Mann <contact@isaiahmann.com>
2017-04-07 11:49:43 -0400

    Beginning to work on new structure for currency panel

---
+++ Assets/Scripts/UI/PickupPup/Currency/SouvenirDisplay.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Author(s): Isaiah Mann
+ * Description: Displays a dog's souvenir
+ * Usage: [no notes]
+ */
+
+using UnityEngine;
+
+public class SouvenirDisplay : CurrencyDisplay
+{
+    [SerializeField]
+    PPUIElement nameDisplay;
+    [SerializeField]
+    PPUIElement descriptionDisplay;
+
+    [SerializeField]
+    PPUIButton displayButton;
+    [SerializeField]
+    PPUIElement hideButton;
+
+}
---
+++ Assets/Scripts/UI/PickupPup/DogProfile/AdoptedDogProfile.cs
@@ -0,0 +1,10 @@
+﻿/*
+ * Author(s): Isaiah Mann
+ * Description: Describes special behaviour only related to adopted dogs
+ * Usage: [no notes]
+ */
+
+public class AdoptedDogProfile : DogShelterProfile 
+{
+
+}
