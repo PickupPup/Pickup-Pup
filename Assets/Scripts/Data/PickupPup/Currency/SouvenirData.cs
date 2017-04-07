04f559216dfc2642af3276138e5733628e55ba42
Isaiah Mann <contact@isaiahmann.com>
2017-04-07 11:24:57 -0400

    Adding skeleton of class for souvenirs

---
+++ Assets/Scripts/Data/PickupPup/Currency/SouvenirData.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * Author(s): Isaiah Mann
+ * Description: Describes a souvenir
+ * Usage: [no notes]
+ */
+
+using UnityEngine;
+
+[System.Serializable]
+public class SouvenirData 
+{
+    #region Instance Accessors
+
+    public string Name
+    {
+        get
+        {
+            return this.name;
+        }
+    }
+
+    public string Description
+    {
+        get
+        {
+            return this.description;
+        }
+    }
+
+    #endregion
+
+    [SerializeField]
+    string name;
+    [SerializeField]
+    string description;
+
+    DogDescriptor dog;
+
+}
