c8874fe886a9bd8490c604a7a2a1f54f27c8666f
Isaiah Mann <contact@isaiahmann.com>
2017-04-19 18:48:02 -0400

    Adding dog sprite tool

---
+++ Assets/Scripts/Tools/DogSpriteDisplayTool.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Author(s): Isaiah Mann
+ * Description: [to be added]
+ * Usage: [no notes]
+ */
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DogSpriteDisplayTool : MonoBehaviourExtended 
+{	
+    [SerializeField]
+    Text nameDisplay;
+
+    [SerializeField]
+    Image dogProfilePortrait;
+
+    [SerializeField]
+    Image dogWorldSprite;
+
+    public void Display(DogDescriptor dog)
+    {
+        this.nameDisplay.text = dog.Name;
+        this.dogProfilePortrait.sprite = dog.Portrait;
+        this.dogWorldSprite.sprite = dog.WorldSprite;
+    }
+
+}
---
+++ Assets/Scripts/Tools/DogSpriteTool.cs
@@ -0,0 +1,49 @@
+﻿/*
+ * Author(s): Isaiah Mann
+ * Description: [to be added]
+ * Usage: [no notes]
+ */
+
+using UnityEngine;
+
+public class DogSpriteTool : MonoBehaviourExtended 
+{	
+    [SerializeField]
+    DogSpriteDisplayTool displayPrefab;
+	
+    [SerializeField]
+    RectTransform viewParent;
+
+    [SerializeField]
+    float widthPerDisplay = 400;
+
+    #region MonoBehaviourExtended Overrides
+
+    protected override void fetchReferences()
+    {
+        base.fetchReferences();
+        displayAllDogs();
+    }
+
+    #endregion
+
+    void displayAllDogs()
+    {
+        DogDatabase data = DogDatabase.GetInstance;
+        int numDogs = data.Dogs.Length;
+        viewParent.sizeDelta = Vector2.one + Vector2.right * numDogs * widthPerDisplay;
+        foreach(DogDescriptor dog in data.Dogs)
+        {
+            addDogToView(dog);
+        }
+    }
+
+    void addDogToView(DogDescriptor dog)
+    {
+        DogSpriteDisplayTool display = Instantiate(displayPrefab);
+        display.Display(dog);
+        display.transform.SetParent(viewParent);
+        display.transform.localScale = Vector3.one;
+    }
+
+}
