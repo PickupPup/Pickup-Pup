f05ded829bbdc5a7bbd565836600da679248a599
bpage-gamedev <bpage.gamedev@gmail.com>
2017-04-25 02:31:33 -0400

    Update Food Player Prefs
    Each DogFoodTyps saves a currentAmount to playerPrefs as they are
    updated. on the CurrencyDisplay gameObject. I rewrote my logic a couple
    times so I have to do some cleaning up.

--- Assets/Scripts/UI/PickupPup/Currency/CurrencyDisplay.cs
+++ Assets/Scripts/UI/PickupPup/Currency/CurrencyDisplay.cs
@@ -1,29 +1,29 @@
 ﻿/*
- * Authors: Grace Barrett-Snyder, Isaiah Mann
+ * Authors: Grace Barrett-Snyder, Isaiah Mann, Ben Page
  * Description: Controls UI of Currency
  */
 
 using UnityEngine;
 using UnityEngine.UI;
 
-public class CurrencyDisplay : PPUIElement 
+public class CurrencyDisplay : PPUIElement
 {
     [SerializeField]
     Image iconImage;
 
-	CurrencyType type;
+    CurrencyType type;
     DogFoodType dogFoodType;
     CurrencyDisplay[] dogFoodDisplays;
 
     public void Init(CurrencyData currency, PPDataController dataController)
-	{
+    {
         this.dataController = dataController;
-		unsubscribeEvents();
-		type = currency.Type;
+        unsubscribeEvents();
+        type = currency.Type;
         iconImage.sprite = currency.Icon;
         updateAmount(currency.Amount);
-		this.dataController.SubscribeToCurrencyChange(type, updateAmount);
-	}
+        this.dataController.SubscribeToCurrencyChange(type, updateAmount);
+    }
 
     public void Init(CurrencyData currency, PPDataController dataController, DogFoodType _dogFoodType, CurrencyDisplay[] _dogFoodDisplays)
     {
@@ -34,41 +34,48 @@ public class CurrencyDisplay : PPUIElement
         dogFoodDisplays = _dogFoodDisplays;
         // TODO: Have to add sprite to FoodItem JSON (or just use color somehow?)
         // iconImage.sprite = FoodDatabase.Instance.Items[(int)dogFoodType].sprite;
-        updateFoodAmount(FoodDatabase.Instance.Food[(int)dogFoodType].CurrentAmount);
+        initFoodAmount();
+        //updateFoodAmount(FoodDatabase.Instance.Food[(int)dogFoodType].CurrentAmount);
         this.dataController.SubscribeToCurrencyChange(type, updateFoodAmount);
     }
 
     protected override void cleanupReferences()
-	{
-		base.cleanupReferences();
-		if(dataController)
-		{
+    {
+        base.cleanupReferences();
+        if (dataController)
+        {
             Debug.Log(type);
-			dataController.UnsubscribeFromCurrencyChange(type, updateAmount);
-		}
-	}
+            dataController.UnsubscribeFromCurrencyChange(type, updateAmount);
+        }
+    }
 
-	protected override void subscribeEvents()
-	{
-		base.subscribeEvents();
-	}
+    protected override void subscribeEvents()
+    {
+        base.subscribeEvents();
+    }
 
-	protected override void unsubscribeEvents()
-	{
-		base.unsubscribeEvents();
-	}
+    protected override void unsubscribeEvents()
+    {
+        base.unsubscribeEvents();
+    }
 
-	public void updateAmount(int newAmount)
-	{
-		text.text = newAmount.ToString();
-	}
+    public void updateAmount(int newAmount)
+    {
+        text.text = newAmount.ToString();
+    }
 
     public void updateFoodAmount(int newAmount)
     {
         //PlayerPrefs.GetInt(type.ToString() + ".currentAmount")
         newAmount = FoodDatabase.Instance.Food[(int)dogFoodType].CurrentAmount;
         PlayerPrefs.SetInt(dogFoodType.ToString() + ".currentAmount", newAmount);
-        Debug.Log(dogFoodType.ToString() + ".currentAmount");
+        text.text = newAmount.ToString();
+    }
+
+    public void initFoodAmount()
+    {
+        int newAmount = PlayerPrefs.GetInt(dogFoodType.ToString() + ".currentAmount", 0);
+        FoodDatabase.Instance.Food[(int)dogFoodType].CurrentAmount = newAmount;
         text.text = newAmount.ToString();
     }
 }
