4110fab99f241003893b0ab20f0adfc267b9bf80
Isaiah Mann <contact@isaiahmann.com>
2017-06-20 15:32:33 -0400

    Updating IAP plugin

--- Assets/Plugins/UnityPurchasing/script/PurchasingCheck.cs
+++ Assets/Plugins/UnityPurchasing/script/PurchasingCheck.cs
@@ -1,5 +1,3 @@
-﻿/*
-#if !UNITY_PURCHASING
-#error "Unity IAP plugin is installed, but Unity IAP is not enabled. Please enable Unity IAP in the Services window."
+﻿#if !UNITY_PURCHASING
+#warning "Unity IAP plugin is installed, but Unity IAP is not enabled. Please enable Unity IAP in the Services window."
 #endif
-*/
